5c566ba90b0246fc8a122d1f48d5c4916743f95b
Hector Vega <linnk99@gmail.com>
2020-12-29 23:04:49 -0500

    Switch structure used in the calculator operators

--- calculadora.go
+++ calculadora.go
@@ -13,7 +13,8 @@ func main() {
 	scanner.Scan()
 	operacion := scanner.Text()
 	fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
+	operador := "p"
+	valores := strings.Split(operacion, operador)
 	fmt.Println(valores)
 	fmt.Println(valores[0] + valores[1])
 	operador1, err1 := strconv.Atoi(valores[0])
@@ -24,5 +25,16 @@ func main() {
 	}
 	operador2, _ := strconv.Atoi(valores[1])
 
-	fmt.Println(operador1 + operador2)
+	switch operador {
+	case "+":
+		fmt.Println(operador1 + operador2)
+	case "-":
+		fmt.Println(operador1 - operador2)
+	case "*":
+		fmt.Println(operador1 * operador2)
+	case "/":
+		fmt.Println(operador1 / operador2)
+	default:
+		fmt.Println(operador, "No esta soportado")
+	}
 }
